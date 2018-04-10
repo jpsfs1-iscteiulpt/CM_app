62427463c3ba753d96f2101c262a1cc50a7f9bae
jpsfs1-iscteiulpt <jpsfs1@iscte-iulpt>
2018-04-10 20:19:48 +0100

    APP CM

---
+++ src/main/java/com/example/jfern/cm_app/Welcome.java
@@ -0,0 +1,37 @@
+package com.example.jfern.cm_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button buttonLogin= (Button) findViewById(R.id.login_btn);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button buttonSignup= (Button) findViewById(R.id.signup_btn);
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
