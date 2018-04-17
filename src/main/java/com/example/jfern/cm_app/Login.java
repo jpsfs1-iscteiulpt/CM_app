0627bd041bdd6170129c0faf6b16340b6b5edbaf
jpsfs1-iscteiulpt <jpsfs1@iscte-iulpt>
2018-04-17 19:57:42 +0100

    APP CM

--- src/main/java/com/example/jfern/cm_app/Login.java
+++ src/main/java/com/example/jfern/cm_app/Login.java
@@ -1,14 +1,28 @@
 package com.example.jfern.cm_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonLogin= (Button) findViewById(R.id.button_do_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
 
 
     }
